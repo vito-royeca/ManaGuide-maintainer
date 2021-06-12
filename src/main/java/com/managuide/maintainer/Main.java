f15aae003e47161d3b99bf82509a82f7575e307b
Vito Royeca <vito.royeca@gmail.com>
2021-06-12 12:51:26 -0400

    Removed the Java code

--- src/main/java/com/managuide/maintainer/Main.java
+++
@@ -1,8 +0,0 @@
-package com.managuide.maintainer;
-
-public class Main {
-    public static void main(String[] args) {
-        Maintainer maintainer = new Maintainer();
-        maintainer.checkServerInfo();
-    }
-}
