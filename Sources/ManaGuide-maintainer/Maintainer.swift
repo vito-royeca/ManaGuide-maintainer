2b1a8563ddd44178decf47fa79ac801696dfb737
Vito Royeca <vito.royeca@gmail.com>
2022-10-23 12:43:04 -0400

    Keyrune updates

--- Sources/ManaGuide-maintainer/Maintainer.swift
+++ Sources/ManaGuide-maintainer/Maintainer.swift
@@ -38,7 +38,7 @@ class Maintainer {
     var rulingsRemotePath  = ""
     let setsRemotePath     = "https://api.scryfall.com/sets"
     let keyruneRemotePath  = "https://keyrune.andrewgioia.com/cheatsheet.html"
-    let rulesRemotePath    = "https://media.wizards.com/2022/downloads/MagicCompRules 20220708.txt"
+    let rulesRemotePath    = "https://media.wizards.com/2022/downloads/Comprehensive Rules 20221007.txt"
 
     // local file names
     var bulkDataLocalPath  = ""
