aca004fd882f4c2d4039325b6872a1797b4cde27
Vito Royeca <vito.royeca@gmail.com>
2022-12-11 12:17:15 -0500

    Keyrune updates

--- Package.swift
+++ Package.swift
@@ -13,8 +13,9 @@ let package = Package(
         .package(url: "https://github.com/tid-kijyun/Kanna.git", from: "5.2.2"),
         .package(url: "https://github.com/codewinsdotcom/PostgresClientKit", from: "1.0.0"),
         .package(url: "https://github.com/mxcl/PromiseKit", from: "7.0.0-rc1"),
-        .package(url: "https://github.com/apple/swift-argument-parser", from: "0.0.1"),
-        .package(url: "https://github.com/apple/swift-tools-support-core.git", from: "0.0.1"),
+        .package(url: "https://github.com/apple/swift-argument-parser", from: "0.2.7"),
+        .package(url: "https://github.com/apple/swift-tools-support-core.git", from: "0.2.7"),
+        
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
