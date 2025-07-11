4b11ccf1c04e57ca5e79fb6470dc5d441c573eb2
Vito Royeca <vito.royeca@gmail.com>
2025-07-11 17:49:42 -0400

    Added processServerReindex()

--- Sources/ManaGuide-maintainer/Maintainer+ServerUpdate.swift
+++ Sources/ManaGuide-maintainer/Maintainer+ServerUpdate.swift
@@ -14,8 +14,29 @@ extension Maintainer {
         try await exec(query: query, with: parameters)
     }
     
+    func processServerReindex() async throws {
+        print("processServerReindex()...")
+        try await exec(query: "REINDEX INDEX cmartist_name_index;")
+        try await exec(query: "REINDEX INDEX cmcard_cmartist_index;")
+        try await exec(query: "REINDEX INDEX cmcard_cmframe_index;")
+        try await exec(query: "REINDEX INDEX cmcard_cmlanguage_index;")
+        try await exec(query: "REINDEX INDEX cmcard_cmlayout_index;")
+        try await exec(query: "REINDEX INDEX cmcard_cmrarity_index;")
+        try await exec(query: "REINDEX INDEX cmcard_cmset_index;")
+        try await exec(query: "REINDEX INDEX cmcard_cmwatermark_index;")
+        try await exec(query: "REINDEX INDEX cmcard_collector_number_index;")
+        try await exec(query: "REINDEX INDEX cmcard_id_index;")
+        try await exec(query: "REINDEX INDEX cmcard_name_index;")
+        try await exec(query: "REINDEX INDEX cmcard_new_id_index;")
+        try await exec(query: "REINDEX INDEX cmcard_tcgplayer_id_index;")
+        try await exec(query: "REINDEX INDEX cmcardtype_name_index;")
+        try await exec(query: "REINDEX INDEX cmlanguage_code_index;")
+        try await exec(query: "REINDEX INDEX cmset_card_count_index;")
+        try await exec(query: "REINDEX INDEX cmset_code_index;")
+    }
+
     func processServerVacuum() async throws {
         print("processServerVacuum()...")
-        try await exec(query: "VACUUM FULL ANALYZE")
+        try await exec(query: "VACUUM FULL ANALYZE;")
     }
 }
